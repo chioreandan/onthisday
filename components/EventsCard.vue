c07169d9ab1719875bc87202c56d485ab56627bd
Chiorean Dan <chiorean.dan12@gmail.com>
2020-12-01 21:18:37 +0200

    add lorem ipsum card

--- components/EventsCard.vue
+++ components/EventsCard.vue
@@ -13,6 +13,24 @@
         </b-col>
       </b-row>
     </b-card>
+    <b-card bg-variant="dark" text-variant="white" v-for="event in events" :key="event.description" class="mb-2 border border-primary">
+      <b-card-title>
+        <h1> <strong> 2020 </strong> </h1>
+      </b-card-title>
+      <b-card-text class="card-text-wrapper">
+        <strong>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis risus eu molestie tempo </strong>
+      </b-card-text>
+      <b-row>
+        <b-col cols="6">
+          <a href="google.com" target="_blank">Lorem ipsum</a>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="6">
+          <a href="google.com" target="_blank">Lorem ipsum</a>
+        </b-col>
+      </b-row>
+    </b-card>
   </div>
 </template>
 
